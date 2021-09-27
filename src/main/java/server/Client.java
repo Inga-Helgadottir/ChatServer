465315ca604e1483e4720e6bb5e971e704c5b554
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-27 15:10:01 +0200

    Startd making Client class made a function to listen for messsages made a function to close everything when done
    xit
    git commit -m

---
+++ src/main/java/server/Client.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private Scanner scan;
+    private PrintWriter pw;
+    private String username;
+
+    public Client(Socket socket, String username)  {
+        try {
+            this.socket = socket;
+            this.pw = new PrintWriter(socket.getOutputStream(), true);
+            this.scan = new Scanner(socket.getInputStream());
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, pw, scan);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        //TODO: change so it checks the hardcoded users
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username for the group chat: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", 8080);
+        Client client = new Client(socket, username);
+        client.listenForMsgs();
+        client.sendMsg();
+    }
+
+    public void sendMsg(){
+    }
+
+    public void listenForMsgs(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msgFromGroupChat;
+                while(socket.isConnected()){
+                    try{
+                        msgFromGroupChat = scan.nextLine();
+                        pw.println(msgFromGroupChat);
+                    }catch(Exception e){
+                        closeEverything(socket, pw, scan);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void closeEverything(Socket socket, PrintWriter pw, Scanner scan) {
+        try {
+            if(pw != null){
+                pw.close();
+            }
+            if(scan != null){
+                scan.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
