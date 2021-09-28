d1deb5a1b77e59191173cc5e1df02486fdbc0e10
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-28 09:49:01 +0200

    added dispatcher

--- src/main/java/server/Dispatcher.java
+++ src/main/java/server/Dispatcher.java
@@ -1,2 +1,24 @@
-package server;public class Dispatcher {
+package server;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Dispatcher  implements Runnable {
+    BlockingQueue<String> messages;
+
+    public Dispatcher(BlockingQueue<String> queue) {
+        this.messages = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String outmsg = "";
+            while (true) {
+                outmsg = messages.take();
+            }
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+    }
 }
