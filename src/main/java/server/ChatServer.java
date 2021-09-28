ba330aa44f6d2be58558695050581189284e40d3
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-28 09:47:16 +0200

    flyttede koden fra server klassen til ChatServer klassen

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,13 +1,44 @@
 package server;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ChatServer {
+    private ServerSocket ss;
+    public ChatServer(ServerSocket ss){
+        this.ss = ss;
+    }
+
+    public void startServer(){
+        while(!ss.isClosed()) {
+            try {
+                Socket s = ss.accept();
+                System.out.println("A new client has connected!");
+                ClientHandler ch = new ClientHandler(s);
+
+                Thread t = new Thread(ch);
+                t.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void closeServerSocket(){
+        try {
+            if(ss != null){
+                ss.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     //Call server with arguments like this: 8088
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
         int port = 8088;
         
         try {
@@ -22,7 +53,8 @@ public class ChatServer {
             return;
         }
 
+        ServerSocket serverSocket = new ServerSocket(port);
+        ChatServer server = new ChatServer(serverSocket);
+        server.startServer();
     }
-
-
 }
